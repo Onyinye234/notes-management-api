b8ac9b19c61bfa475b386ec9c53fb28ba97b29c9
Onyinyechi <ayituonyinyechi@gmail.com>
2026-03-09 05:01:31 -0700

    Intial commit:project setup with Springboot dependencies

---
+++ notepad-api/src/main/java/com/notepad/notepad_api/NotepadApiApplication.java
@@ -0,0 +1,13 @@
+package com.notepad.notepad_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotepadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotepadApiApplication.class, args);
+	}
+
+}
---
+++ notepad-api/src/test/java/com/notepad/notepad_api/NotepadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notepad.notepad_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotepadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
